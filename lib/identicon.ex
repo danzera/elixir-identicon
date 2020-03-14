cc45b5f60f5d3594cefc9ecfeb2398bc4d43a0ac
Dan Zera <dczera@gmail.com>
2020-03-13 18:47:07 -0700

    add image struct, method for picking rgb color value based on the string input, and add tests

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,8 +7,8 @@ defmodule Identicon do
   """
   def main(input) do
 		input # "pipe" input into the hash_input method
-		|> hash_input
-		# |> function_three
+		|> hash_input # will automatically pass the rusulting struct onto pick_color
+		|> pick_color
 		# |> function_four
 	end
 
@@ -17,12 +17,27 @@ defmodule Identicon do
 
 		## Examples
 			iex> Identicon.hash_input("apple")
-			[31, 56, 112, 190, 39, 79, 108, 73, 179, 227, 26, 12, 103, 40, 149, 127]
+			%Identicon.Image{
+				hex: [31, 56, 112, 190, 39, 79, 108, 73, 179, 227, 26, 12, 103, 40, 149, 127]
+			}
 
   """
 	def hash_input(input) do
-		:crypto.hash(:md5, input)
+		hex = :crypto.hash(:md5, input)
 		|> :binary.bin_to_list
+
+		%Identicon.Image{hex: hex}
+	end
+
+	def pick_color(image) do
+		# pattern matching of Identicon.Image structs, then pulling off the first 3 values from the hex property of the image argument
+		%Identicon.Image{hex: [r, g, b | _tail]} = image # hex property from image gets pattern matched to [r, g, b | _tail]
+		# above step done more verbosely:
+		# %Identicon.Image{hex: hex_list} = image
+		# [r, g, b | _tail] = hex_list
+
+ 		# using a tuple for {r, g, b} because each value has a specific meaning
+		%{image | color: {r, g, b}} # equivalent to Map.put(image, :color, {r, g, b})
 	end
 
 end
--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -5,6 +5,14 @@ defmodule IdenticonTest do
 	test "hash banana" do
 		input = "banana"
 		hashed_input = [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
-		assert Identicon.hash_input(input) == hashed_input
-  end
+		assert Identicon.hash_input(input) == %Identicon.Image{hex: hashed_input}
+	end
+	
+	test "squish banana...into rgb value (first three of its hex value)" do
+		input = "banana"
+		hashed_input = [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+		[a, b, c | _tail] = hashed_input
+		image_banana = Identicon.hash_input(input)
+		assert Identicon.pick_color(image_banana) == [a, b, c]
+	end
 end
